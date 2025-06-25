9856278e2d1d39e9fba398578683ad982da176dd
suman shradha <shradhasuman852216@gmail.com>
2025-06-25 09:55:14 +0530

    changes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,22 +1,18 @@
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Card(props) {
-  const navigate = useNavigate();
-
-  function handleClick() {
-    navigate(`/country/${props.name}`, {
-      state: props.countryData,
-    });
-  }
-
   return (
-    <div className="card-container" onClick={handleClick}>
-      <img src={props.src} />
+    <Link
+      to={`/country/${props.name}`}
+      state={props.countryData}
+      className="card-container"
+    >
+      <img src={props.src} alt={`Flag of ${props.name}`} />
       <h3>{props.name}</h3>
-      <h4>Population : {props.Population}</h4>
-      <h4>Region : {props.Region}</h4>
-      <h4>Capital : {props.Capital}</h4>
-    </div>
+      <h4>Population: {props.Population}</h4>
+      <h4>Region: {props.Region}</h4>
+      <h4>Capital: {props.Capital}</h4>
+    </Link>
   );
 }
 
